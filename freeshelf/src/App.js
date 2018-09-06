f762b09cf040bb56aedc603636c6d89ceb2d719f
Alex Corey <alex.swansboro@gmail.com>
2018-09-06 16:41:53 -0400

    Books appearing on page

---
+++ freeshelf/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import 'bulma/css/bulma.css'
+import './App.css'
+import books from './books.json'
+import Book from './Book'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      books: books
+
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        {this.state.books.map((book, idx) =>
+          <Book key={idx} book={book} />)}
+      </div>
+    )
+  }
+}
+//   bookExpanded (){
+//     <p className=''
+//   }
+// }
+export default App
---
+++ freeshelf/src/Book.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+class Book extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      collapsed: true
+    }
+  }
+  render () {
+    const { book } = this.props
+    return (
+      <div className='books'>
+        <div id='collapsed-view'>
+          <a href={book.url} className='title' target='_blank'>{book.title}</a>
+          <p className='author'>{book.author}</p>
+          <div className='bookImage'>
+            <img src={book.coverImageUrl} alt='book picture' />
+          <div className='shortDescription'>{book.shortDescription}</div>
+        </div>
+        <div id='expanded-view'>
+          <a href={book.url} className='url' target='_blank'><strong>URL: </strong>{book.url}</a>
+          <div className='publisher'><strong>Publisher: </strong>{book.publisher}</div>
+          <div className='publicationDate'><strong>Publication Date: </strong>{book.publicationDate}</div>
+          <div className='detailedDescription'><strong>Description: </strong>{book.detailedDescription}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Book
